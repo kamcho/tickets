8d25620e09369d715f8ca91478b98f1b31c0aec5
kamcho <njokevin9>
2026-06-02 14:43:46 +0300

    pay

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MyUser, TicketCategory, Customer, Ticket, TicketAssignment, TicketComments, TicketAttachments
 
-# Register your models here.
+admin.site.register(MyUser)
+admin.site.register(TicketCategory)
+admin.site.register(Customer)
+admin.site.register(Ticket)
+admin.site.register(TicketAssignment)
+admin.site.register(TicketComments)
+admin.site.register(TicketAttachments)
