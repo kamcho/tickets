5b47d5a93578e09f6e9593a24231ceb8c5ba43f2
kamcho <njokevin9>
2026-06-02 12:01:10 +0300

    rqs

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-# Add the project's root folder to the Python search path.
-# This ensures Passenger can find Django and core application files.
-project_dir = os.path.dirname(os.path.abspath(__file__))
-if project_dir not in sys.path:
-    sys.path.insert(0, project_dir)
+# # Add the project's root folder to the Python search path.
+# # This ensures Passenger can find Django and core application files.
+# project_dir = os.path.dirname(os.path.abspath(__file__))
+# if project_dir not in sys.path:
+#     sys.path.insert(0, project_dir)
 
-# Set the environment variable pointing to the Django settings file.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticket.settings")
+# # Set the environment variable pointing to the Django settings file.
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticket.settings")
 
 # Import the standard WSGI application handler.
 # Passenger hooks into this 'application' object to route requests.
